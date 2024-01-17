8f35adf5a5aa49224f644f65b5d67b2a5a59d34f
Mohlabe <maphallemohlabe@gmail.com>
2024-01-17 12:49:24 +0200

    Initial

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # HRChatApp
 # Project HRChattApp
 This project is to dockerize and deploy a chat application for HR depatrment
+
+Terraform Commands
+// Init Terraform
+terraform init
+
+// Review Plan
+terraform plan
+
+// Run Terraform Script
+terraform apply
+
+// Destroy Changes
+terraform destroy
--- main.tf
+++ main.tf
@@ -13,40 +13,58 @@ provider "aws" {
   
 }
 
-resource "aws_instance" "AppContainer" {
+
+
+# Generating Key pairs
+resource "tls_private_key" "rsa_pairs" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {}
+
+resource "aws_key_pair" "KeyPair_Deploy" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_pairs.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+
+  content = tls_private_key.rsa_pairs.private_key_pem
+  filename = var.key_name
+
+}
+
+resource "aws_security_group" "App_sg" {
+  name        = "App_sg"
+  vpc_id = "vpc-02884d1aebf7d6bbf"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "AppServer" {
+  count = 2
   ami           = "ami-0e878fcddf2937686"
   instance_type = "t3.micro"
+  subnet_id = "subnet-0971000f20d012cd1"
+  key_name = aws_key_pair.KeyPair_Deploy.key_name
 
    tags = {
-    Name = "HrApp"
+    Name = "Instance-${count.index + 1}"
+
+  
 }
-}
-
-# Create a VPC
-resource "aws_vpc" "HrApp_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-tags = {
-    Name = "HrApp"
-
-}
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.HrApp_vpc.id
-  cidr_block        = "172.31.32.0/20"
-  availability_zone = "af-south-1"
-
-  tags = {
-    Name = "HrApp"
-  }
-}
-
-resource "aws_network_interface" "Nic11" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.31.32.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
 }
