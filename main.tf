44e6cfe83cea5b3520dc6f6900a6b1f8f183c7ae
Mohlabe <maphallemohlabe@gmail.com>
2024-01-21 01:48:48 +0200

    newpull

--- main.tf
+++ main.tf
@@ -35,9 +35,9 @@ resource "local_file" "private_key" {
 
 }
 
-resource "aws_security_group" "App_sg" {
-  name        = "App_sg"
-  vpc_id = "vpc-02884d1aebf7d6bbf"
+resource "aws_security_group" "projectApp_sg" {
+  name        = "projectApp_sg"
+  vpc_id = "vpc-0ef80f67db66fb446"
 
   ingress {
     from_port   = 22
@@ -55,15 +55,15 @@ resource "aws_security_group" "App_sg" {
 }
 
 
-resource "aws_instance" "AppServer" {
+resource "aws_instance" "HrAppServer" {
   count = 2
   ami           = "ami-0e878fcddf2937686"
   instance_type = "t3.micro"
-  subnet_id = "subnet-0971000f20d012cd1"
+  subnet_id = "subnet-03dfd1000114782e3"
   key_name = aws_key_pair.KeyPair_Deploy.key_name
 
    tags = {
-    Name = "Instance-${count.index + 1}"
+    Name = "JHBAppserver-${count.index + 1}"
 
   
 }
